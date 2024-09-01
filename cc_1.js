b81f9c28afc4a17bbb60e6b48784ce653ac9fbaf
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-09-01 15:21:47 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Jane Doe";
+const employeeID = 112;
+var isActive = true
+
+console.log(typeof(employeeName));
+console.log(typeof(employeeID));
+console.log(typeof(isActive));
